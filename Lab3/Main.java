1cfd80b48b86e40d4c0e1f967f754cfd707ce071
nambp1807 <49878264+nambp1807@users.noreply.github.com>
2019-10-13 21:43:43 +0700

    Add files via upload

---
+++ Lab3/Main.java
@@ -0,0 +1,12 @@
+package Lab3;
+
+public class Main {
+    public static void main(String[] args) {
+        PhanSo ps = new PhanSo();
+        ps.inputInfo();
+        ps.InPhanSo();
+        ps.toiGianPhanSo();
+        ps.NghichDao();
+//        ps.rutGonPhanSo();
+    }
+}
