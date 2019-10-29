8b2c8bacb6a371aff1319ab9f491ab60f38df60d
nambp1807 <49878264+nambp1807@users.noreply.github.com>
2019-10-29 22:35:16 +0700

    nộp bài tập assgnment7

---
+++ assignment7/Main.java
@@ -0,0 +1,14 @@
+package assignment7;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ListCustomer list1 = new ListCustomer();
+        list1.addCusVN();
+        list1.addCusIN();
+        list1.showlistCustomer();
+        list1.countCus();
+        list1.tienTBnuocngoai();
+    }
+}
