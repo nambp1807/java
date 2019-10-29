bee0ec9e47e6e7a722339bc0c1e810010562032d
nambp1807 <49878264+nambp1807@users.noreply.github.com>
2019-10-29 22:38:16 +0700

    nộp bài tập labsession7

---
+++ labsession7/Student.java
@@ -0,0 +1,64 @@
+package labsession7;
+
+public class Student {
+     public int id;
+     public String name;
+     public int age;
+     public String address;
+     public double gpa;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, int age, String address, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.gpa = (float) gpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = (float) gpa;
+    }
+
+
+
+
+}
